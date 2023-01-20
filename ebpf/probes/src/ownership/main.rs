867f1cec73b78d46c5d181d22ecaec51537666cd
Jobs Wang <wchswchs@gmail.com>
2023-01-20 21:55:23 +0800

    fix

--- ebpf/probes/src/ownership/main.rs
+++ ebpf/probes/src/ownership/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+use core::sync::atomic::AtomicBool;
+
 use probes::ownership::*;
 use redbpf_probes::kprobe::prelude::*;
 use std_ownership::rcm::center::ResourceCenter;
+use std_ownership_api::model::Owner;
 
 program!(0xFFFFFFFE, "GPL");
 
@@ -14,12 +17,13 @@ static mut BORROW_RESULT: PerfMap<BorrowResult> = PerfMap::with_max_entries(1024
 fn do_borrow(regs: Registers) {
     let rc = ResourceCenter::build();
     unsafe {
-        let applier = regs.parm1();
+        let applier_id = regs.parm1();
         let resource_id = regs.parm2() as *const u8;
         let data = regs.parm3() as *const &[u8];
     }
+    let applier = Applier::new(applier_id, role);
     let borrow_state = rc::borrow(applier, resource_id, data);
-    let borrow_result = BorrowResult::new(applier, borrow_state);
+    let borrow_result = BorrowResult::new(applier_id, borrow_state);
     unsafe {
         BORROW_RESULT.insert(regs.ctx, &borrow_result);
     }
