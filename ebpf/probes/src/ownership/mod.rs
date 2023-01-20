51d38426401073406d4186a9db2a197b64958a31
Jobs Wang <wchswchs@gmail.com>
2023-01-20 21:55:11 +0800

    fix

--- ebpf/probes/src/ownership/mod.rs
+++ ebpf/probes/src/ownership/mod.rs
@@ -1,12 +1,37 @@
+use std_ownership_api::model::{Owner, Role};
+
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct BorrowResult {
-    applier: str,
+    applier: u8,
     state: bool,
 }
 
 impl BorrowResult {
-    pub fn new(&self, applier: str, state: bool) -> BorrowResult {
+    pub fn new(applier: u, state: bool) -> BorrowResult {
         BorrowResult { applier, state }
     }
+}
+
+pub struct Applier {
+    id: u8,
+    role: Role
+}
+
+impl Applier {
+    #[must_use]
+    pub fn new(id: u8, role: Role) -> Applier {
+        Applier { id, role }
+    }
+}
+
+impl Owner for Applier {
+    #[inline]
+    fn id(&self) -> u8 {
+        self.id
+    }
+    #[inline]
+    fn role(&self) -> Role {
+        self.role
+    }    
 }
